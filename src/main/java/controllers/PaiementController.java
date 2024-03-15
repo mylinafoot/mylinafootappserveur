befce99b8aafd432489b63a7d16d1f43b2bc1e19
joellungu <lungujoel138@gmail.com>
2024-03-15 18:17:01 +0100

    prod f 1

--- src/main/java/controllers/PaiementController.java
+++ src/main/java/controllers/PaiementController.java
@@ -40,6 +40,7 @@ public class PaiementController {
         String rep = verifOTP(billet.get("otp").asText(), billet.get("referencenumber").asText());
         JsonNode jsonNode = objectMapper.readTree(rep);
         if(jsonNode.get("respcodedesc").asText().contains("Approuvé")) {
+            //if(true) {
             //Text a revoire
             List<HashMap> billes = new LinkedList<>();
             //
@@ -50,8 +51,8 @@ public class PaiementController {
             for (int t = 0; t < billet.get("nombrePlace").asInt(); t++) {
                 Billet bi = new Billet();
                 bi.idMatch = billet.get("id").asLong();
-                bi.qrCode = billet.get("qrCode").asText();
-                bi.typePlace = billet.get("typePlace").asText();
+                bi.qrCode = billet.get("qrcode").asText();
+                bi.typePlace = billet.get("place").asText();
                 bi.journee = billet.get("journee").asText();
                 bi.nomEquipeA = billet.get("nomEquipeA").asText();
                 bi.nomEquipeB = billet.get("nomEquipeB").asText();
@@ -104,6 +105,7 @@ public class PaiementController {
         }else{
             //
             return Response.status(404).entity(rep).build();
+            //return Response.status(404).entity("rep").build();
         }
 
     }
@@ -130,7 +132,7 @@ public class PaiementController {
             for (int t = 0; t < (Integer) billet.get("nombrePlace"); t++) {
                 Billet bi = new Billet();
                 bi.idMatch = (Long) billet.get("id");
-                bi.qrCode = (String) billet.get("qrCode");
+                bi.qrCode = (String) billet.get("qrcode");
                 bi.typePlace = (String) billet.get("typePlace");
                 bi.journee = (String) billet.get("journee");
                 bi.nomEquipeA = (String) billet.get("nomEquipeA");
@@ -139,34 +141,8 @@ public class PaiementController {
                 bi.heure = (String) billet.get("heure");
                 bi.stade = (String) billet.get("stade");
                 //----------------------------------------------------------------
-                if(billet.get("place").equals("Pourtour")){
 
-                    int nPlace = (Integer) billet.get("nombrePlace");
 
-                    match.nombreDePlacesPourtour = match.nombreDePlacesPourtour - nPlace;
-
-                }
-                if(billet.get("place").equals("Tribune Lateralle")){
-                    int nPlace = (Integer) billet.get("nombrePlace");
-
-                    //if(match.nombreDePlacesTribuneLateralle
-                    match.nombreDePlacesTribuneLateralle = match.nombreDePlacesTribuneLateralle - nPlace;
-
-                }
-                if(billet.get("place").equals("Tribune Honneur")){
-                    int nPlace = (Integer) billet.get("nombrePlace");
-
-                    //if(match.nombreDePlacesTribuneHonneur
-                    match.nombreDePlacesTribuneHonneur = match.nombreDePlacesTribuneHonneur - nPlace;
-
-                }
-                if(billet.get("place").equals("Tribune Centrale")){
-                    int nPlace = (Integer) billet.get("nombrePlace");
-
-                    //if(match.nombreDePlacesTribuneHonneur
-                    match.nombreDePlacesTribuneHonneur = match.nombreDePlacesTribuneHonneur - nPlace;
-
-                }
                 //----------------------------------------------------------------
                 //bi.qrCode = (String) billet.get("qrCode");
                 //bi.qrCode = (String) billet.get("qrCode");
@@ -260,7 +236,7 @@ public class PaiementController {
         //return Response.ok(response.body()).build();
     }
 
-    private String verifOTP(String otp, String rrn) throws URISyntaxException, IOException, InterruptedException {
+    private String verifOTP2(String otp, String rrn) throws URISyntaxException, IOException, InterruptedException {
         //https://test.new.rawbankillico.com:4003/RAWAPIGateway/ecommerce/payment/770013/000007316065
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI("https://new.rawbankillico.com:4004/RAWAPIGateway/ecommerce/payment/"+otp+"/"+rrn+""))
@@ -280,7 +256,7 @@ public class PaiementController {
         return response.body();
     }
 
-    private String sendOTP(String telephone, String devise, double montant ) throws URISyntaxException, IOException, InterruptedException {
+    private String sendOTP2(String telephone, String devise, double montant ) throws URISyntaxException, IOException, InterruptedException {
         //4004
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI("https://new.rawbankillico.com:4004/RAWAPIGateway/ecommerce/payment"))
@@ -300,4 +276,48 @@ public class PaiementController {
         System.out.println("La reponse 1: "+response.body());
         return response.body();
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private String verifOTP(String otp, String rrn) throws URISyntaxException, IOException, InterruptedException {
+        //https://test.new.rawbankillico.com:4003/RAWAPIGateway/ecommerce/payment/770013/000007316065
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("https://new.rawbankillico.com:4004/RAWAPIGateway/ecommerce/payment/"+otp+"/"+rrn+""))
+                .header("LogInName", "9d668476741822a134ab199f33f43576304c9d49fcb3c487774954cd5d31cd06")
+                .header("LogInPass", "30b9cd6d7001dbbc0e724a6946725281f87df6877e8bdd43484c53cb7abe1b82")
+                .header("Authorization", "Basic MzdjMTM1YWJlMWIxOGFhNDJmNDY0NzVkNDA5NzkzNmQzNTk0YzFjYzQxMDZkZGNlNGZlODM3MzgwNjE3Y2RjZToyZGI0ZjRiYTQzYTUyMzAxOTEyMTJhYjgzOWNiYTY5ZmJiOTNmM2Q0NmUwZjQwNjM3M2M1MjMyZDZjNmUzM2I1")
+                .GET().build();
+        //
+        HttpResponse<String> response = HttpClient
+                .newBuilder()
+                .proxy(ProxySelector.getDefault())
+                .build()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        //
+        System.out.println("La reponse 2: "+response.body());
+        return response.body();
+    }
+
+    private String sendOTP(String telephone, String devise, double montant ) throws URISyntaxException, IOException, InterruptedException {
+        //4004
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("https://new.rawbankillico.com:4004/RAWAPIGateway/ecommerce/payment"))
+                .header("LogInName", "9d668476741822a134ab199f33f43576304c9d49fcb3c487774954cd5d31cd06")
+                .header("LogInPass", "30b9cd6d7001dbbc0e724a6946725281f87df6877e8bdd43484c53cb7abe1b82")
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Basic MzdjMTM1YWJlMWIxOGFhNDJmNDY0NzVkNDA5NzkzNmQzNTk0YzFjYzQxMDZkZGNlNGZlODM3MzgwNjE3Y2RjZToyZGI0ZjRiYTQzYTUyMzAxOTEyMTJhYjgzOWNiYTY5ZmJiOTNmM2Q0NmUwZjQwNjM3M2M1MjMyZDZjNmUzM2I1")
+                .POST(HttpRequest.BodyPublishers.ofString("{\r\n\t\"mobilenumber\": \""+telephone+"\",\r\n\t\"trancurrency\":\""+devise+"\",\r\n\t\"amounttransaction\": \""+montant+"\",\r\n\t\"merchantid\": \"brnch0000000000000801\",\r\n\t\"invoiceid\":\"123456715\",\r\n\t\"terminalid\":\"123456789012\",\r\n\t\"encryptkey\": \"AX8dsXSKqWlJqRhpnCeFJ03CzqMsCisQVUNSymXKqeiaQdHf8eQSyITvCD6u3CLZJBebnxj5LbdosC/4OvUtNbAUbaIgBKMC5MpXGRXZdfAlGsVRfHTmjaGDe1RIiHKP\",\r\n\t\"securityparams\":{\r\n\t\t\"gpslatitude\": \"24.864190\",\r\n\t\t\"gpslongitude\": \"67.090420\"\r\n\t}\r\n}"))
+                //.POST(HttpRequest.BodyPublishers.ofString("{\r\n\t\"mobilenumber\": \""+telephone+"\",\r\n\t\"trancurrency\":\""+devise+"\",\r\n\t\"amounttransaction\": \""+montant+"\",\r\n\t\"merchantid\": \"brnch0000000000000801\",\r\n\t\"invoiceid\":\"123456715\",\r\n\t\"terminalid\":\"123456789012\",\r\n\t\"encryptkey\": \"AX8dsXSKqWlJqRhpnCeFJ03CzqMsCisQVUNSymXKqeiaQdHf8eQSyITvCD6u3CLZJBebnxj5LbdosC/4OvUtNbAUbaIgBKMC5MpXGRXZdfAlGsVRfHTmjaGDe1RIiHKP\",\r\n\t\"securityparams\":{\r\n\t\t\"gpslatitude\": \"24.864190\",\r\n\t\t\"gpslongitude\": \"67.090420\"\r\n\t}\r\n}"))
+                .build();
+        //
+        HttpResponse<String> response = HttpClient
+                .newBuilder()
+                .proxy(ProxySelector.getDefault())
+                .build()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        //
+        System.out.println("La reponse 1: "+response.body());
+        return response.body();
+    }
+
+
 }
